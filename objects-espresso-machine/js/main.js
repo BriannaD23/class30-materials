a9b31f74469ee72cf85ee0c73c50a6534c40c1bd
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-10-03 20:30:10 -0700

    first commit

---
+++ objects-espresso-machine/js/main.js
@@ -0,0 +1,25 @@
+//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class expressoMachine{
+    constructor(button, model, coffee,){
+    
+    this.color = color
+    this.model = model
+    this.coffee= coffee
+    this.steam = steam
+
+    }
+
+    brew(){
+        console.log('sound')
+    }
+
+    heat(){
+        console.log('warm')
+    }
+
+    grind(){
+        console.log('grind')
+    }
+
+}
